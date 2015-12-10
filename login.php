047eadbbfe263f3687964ed38a6516a94657dea5
KensukeTakeoka <t.kensk8@gmail.com>
2015-12-10 15:54:03 +0800

    お疲れさまです。new membersの新規登録が入りました。loginの方を名前とパスワードにします。

--- login.php
+++ login.php
@@ -1,7 +1,33 @@
 <?php
 session_start();
 require('dbconnect.php');
+if (empty($_REQUEST['name'])){
+  print('名前を記入してください');
+
+}else{
+  print('正しく記入されています');
+}
+
 var_dump($_POST);
+// if(!isset($_SESSION['join'])){
+//   header('Location: login.php');
+//   exit();
+// }
+if (!empty($_POST)) {
+  //登録処理をする
+  $sql = sprintf('INSERT INTO members SET name="%s", email="%s",password="%s",created="%s"',
+    mysqli_real_escape_string($db, $_POST['name']),
+    mysqli_real_escape_string($db, $_POST['email']),
+    mysqli_real_escape_string($db, $_POST['password']),date('Y-m-d H:i:s'));
+    // mysqli_real_escape_string($db, $_POST['join']['image']), date('Y-m-d H:i:s'));
+  //$imageを代入すること 
+    // echo $sql;
+    mysqli_query($db, $sql) or die(mysqli_error($db));
+    unset($_SESSION['join']);
+    // $_SESSION['join']=$_POST;
+    header('Location: login.php');
+    // exit();
+}
 ?>
 
 <!DOCTYPE html>
