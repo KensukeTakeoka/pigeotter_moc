67d77545726882690afeaf8fac5a0ce1dfb86138
satorukitamoto <otankonasu220@gmail.com>
2015-12-14 21:20:55 +0800

    自動返信機能に期限をつけました

--- plofile.php
+++ plofile.php
@@ -2,33 +2,53 @@
 require('dbconnect.php');
 session_start();
 
- // アクセスカウンタ１
-$fp = @fopen("counter1.txt", "r+") or die("Counter Error");
- flock($fp, LOCK_EX);
- $count = fgets($fp);
- $count++;
- rewind($fp);
- fputs($fp, $count);
- fclose($fp);
-
-		 $sql = sprintf('INSERT INTO members SET count=%d',
-		 	mysqli_real_escape_string($db, $count)
-					);
-
- 		mysqli_query($db, $sql) or die(mysqli_error($db));
-
-// echo $count;
+	 // -----------アクセスカウンタ１-----------
+	$fp = @fopen("counter1.txt", "r+") or die("Counter Error");
+	flock($fp, LOCK_EX);
+	$count = fgets($fp);
+	$count++;
+	rewind($fp);
+	fputs($fp, $count);
+	fclose($fp);
+	//------------------------------------
 
 
- 		$sql = sprintf('SELECT * FROM posts WHERE 1');
-		$record = mysqli_query($db, $sql) or die(mysqli_error($db));
-		$table = mysqli_fetch_assoc($record);
 
-		$message = $table['message'];
+	//---------------データベースからデータを持ってくる---------------------
+	// while(){
+	// 	$sql = sprintf('SELECT * FROM posts WHERE 1');
+	// }
 
-		if($count % 3 == 0 ){ 
-			echo $message;
-		}
+
+
+
+
+	//------------------------------------------------------------
+
+
+	//-----------自動返信機能-----------
+	$randum_id=rand(1,6);
+	$sql = sprintf('SELECT * FROM posts WHERE id='.$randum_id);
+	$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+	$table = mysqli_fetch_assoc($record);
+	$message = $table['message'];
+
+	$sql = sprintf('SELECT * FROM members WHERE id AND modified');
+	$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+	$time = mysqli_fetch_assoc($record);
+
+	// echo $time['modified'];
+	// echo $message;
+
+
+
+	// if($count % 3 == 0 ){
+	// 	if($time['modified'] < $time['modified']+60*60*24*14){
+	// 		echo $message;
+	// 	}
+	// }
+
+	//---------------------------------
 
 ?>
 
@@ -73,9 +93,9 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
   </head>
 
   <body>
-
-    <div class="navbar navbar-default navbar-fixed-top">
-    	<p>あなたは<?php  print $count; ?>羽目のハトです。</p>
+ 
+     <div class="navbar navbar-default navbar-fixed-top">
+    	
     	<div class="container">
 	      	<div class="row">
 	      		<div class="col-lg-4">
@@ -86,23 +106,30 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 				        <span class="icon-bar"></span>
 			        </button>
 			        <div id="favo">
+			        	
 					    <h3>Plofile</h3>		
 					</div>
 			        </div>
 			    </div>
+<!-- 			    <div class="col-lg-2 centered">
+					<p>あなたは<?php  print $count; ?>羽目のハトです。</p>
+				</div> -->
+
 			    <div class="col-lg-4 centered">
+			    	
+			    	<span>あなたは<?php  print $count; ?>羽目のハトです。</span></br>
 			    </div>
 			    <div class="col-lg-4">
 		        	<div class="button1">
-			       		<button type="button" class="btn btn-default btn-lg">
+			       		<a href="index.php"><button type="button" class="btn btn-default btn-lg">
 						  <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
-						</button>
-						<button type="button" class="btn btn-default btn-lg">
+						</button></a>
+						<a href="favorite.php"><button type="button" class="btn btn-default btn-lg">
 						  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-						</button>
-						<button type="button" class="btn btn-default btn-lg">
+						</button></a>
+						<a href="plofile.php"><button type="button" class="btn btn-default btn-lg">
 						  <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-						</button>
+						</button></a>
 					</div>
 				</div>
 
@@ -110,11 +137,30 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 		</div>
     </div><!--/.nav-collapse -->
 
+	<div  class ="row">
+		<div  class="col-lg-12" >
+   			<div class="card hovercard">
+        		<div class="card-background" style="width:100%" >
+        			
+        		</div>
+   
+        		<div class="useravatar">
+            		<img alt="" src="assets/img/hato.jpg" width="100" height="50">
+        		</div>
+
+        		<div class="card-info">
+        			<span class="card-title">はとぽっぽ</span>
+        		</div>
+
+    		</div>
+    	</div>
+	</div>
+
 
 	<div class="containerRe">
-			<div style="margin-bottom: 15px;">
-				<div class="row">
-					<div class="col-lg-2"></div>
+		<div style="margin-bottom: 15px;">
+			<div class="row">
+				<div class="col-lg-2"></div>
 					<div class="col-lg-3 centered">
 						<img src="assets/img/hato.jpg" width="100" height="100" alt="">
 					</div>
@@ -127,9 +173,21 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 							<div class="col-lg-10 centered">
 								<dl id="acMenu">
 									<dt><span class="glyphicon glyphicon-share-alt" style="margin-left:400px;"></span></dt>
-									<dd><textarea class="form-control" rows=""><?php if($count % 3 == 0 ){ echo $message;} ?></textarea>
-										<button class="
-										btnr" type="button"  style="float:right;">Go!</button>
+									<dd><textarea class="form-control" rows="">
+										<?php 
+											if($count % 3 == 0 ){
+												if($time['modified'] < $time['modified']+60*60*24*14){
+													$randum_id=rand(1,6);
+													$sql = sprintf('SELECT * FROM posts WHERE id='.$randum_id);
+													$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+													$table = mysqli_fetch_assoc($record);
+													$message = $table['message'];
+													echo $message;
+												}
+											}
+									 	?>
+									 	</textarea>
+										<button class="btnr" type="button"  style="float:right;">Go!</button>
 									</dd>
 								</dl>
 							</div>
@@ -139,13 +197,13 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 						</div>
 						
 					</div>
-					<div class="col-lg-2"></div>
-				</div>
+				<div class="col-lg-2"></div>
 			</div>
+		</div>
 
-			<div style="margin-bottom: 15px;">
-				<div class="row">
-					<div class="col-lg-2"></div>
+		<div style="margin-bottom: 15px;">
+			<div class="row">
+				<div class="col-lg-2"></div>
 					<div class="col-lg-3 centered">
 						<img src="assets/img/hato.jpg" width="100" height="100" alt="">
 					</div>
@@ -158,7 +216,22 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 							<div class="col-lg-10 centered">
 								<dl id="acMenu">
 									<dt><span class="glyphicon glyphicon-share-alt" style="margin-left:400px;"></span></dt>
-									<dd><textarea class="form-control" rows="" ></textarea></dd>
+									<dd><textarea class="form-control" rows="">
+										<?php
+											if($count % 3 == 0 ){
+												if($time['modified'] < $time['modified']+60*60*24*14){
+													$randum_id=rand(1,6);
+													$sql = sprintf('SELECT * FROM posts WHERE id='.$randum_id);
+													$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+													$table = mysqli_fetch_assoc($record);
+													$message = $table['message'];
+													echo $message;
+												}
+											}
+											?>
+										</textarea>
+										<button class="btnr" type="button"  style="float:right;">Go!</button>
+									</dd>
 								</dl>
 							</div>
 							<div class="col-lg-1 centered">
@@ -167,14 +240,14 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 						</div>
 						
 					</div>
-					<div class="col-lg-2"></div>
-				</div>
+				<div class="col-lg-2"></div>
 			</div>
+		</div>
 
 
-			<div style="margin-bottom: 15px;">
-				<div class="row">
-					<div class="col-lg-2"></div>
+		<div style="margin-bottom: 15px;">
+			<div class="row">
+				<div class="col-lg-2"></div>
 					<div class="col-lg-3 centered">
 						<img src="assets/img/hato.jpg" width="100" height="100" alt="">
 					</div>
@@ -187,7 +260,18 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 							<div class="col-lg-10 centered">
 								<dl id="acMenu">
 									<dt><span class="glyphicon glyphicon-share-alt" style="margin-left:400px;"></span></dt>
-									<dd><textarea class="form-control" rows="" ></textarea></dd>
+									<dd><textarea class="form-control" rows="">
+										<?php 
+											$randum_id=rand(1,6);
+											$sql = sprintf('SELECT * FROM posts WHERE id='.$randum_id);
+											$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+											$table = mysqli_fetch_assoc($record);
+											$message = $table['message'];
+											echo $message;
+										?>
+										</textarea>
+										<button class="btnr" type="button"  style="float:right;">Go!</button>
+									</dd>
 								</dl>
 							</div>
 							<div class="col-lg-1 centered">
@@ -196,14 +280,14 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 						</div>
 						
 					</div>
-					<div class="col-lg-2"></div>
-				</div>
+				<div class="col-lg-2"></div>
 			</div>
+		</div>
 
 
-			<div style="margin-bottom: 15px;">
-				<div class="row">
-					<div class="col-lg-2"></div>
+				<div style="margin-bottom: 15px;">
+			<div class="row">
+				<div class="col-lg-2"></div>
 					<div class="col-lg-3 centered">
 						<img src="assets/img/hato.jpg" width="100" height="100" alt="">
 					</div>
@@ -216,7 +300,18 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 							<div class="col-lg-10 centered">
 								<dl id="acMenu">
 									<dt><span class="glyphicon glyphicon-share-alt" style="margin-left:400px;"></span></dt>
-									<dd><textarea class="form-control" rows="" ></textarea></dd>
+									<dd><textarea class="form-control" rows="">
+										<?php 
+											$randum_id=rand(1,6);
+											$sql = sprintf('SELECT * FROM posts WHERE id='.$randum_id);
+											$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+											$table = mysqli_fetch_assoc($record);
+											$message = $table['message'];
+											echo $message;
+										?>
+										</textarea>
+										<button class="btnr" type="button"  style="float:right;">Go!</button>
+									</dd>
 								</dl>
 							</div>
 							<div class="col-lg-1 centered">
@@ -225,14 +320,13 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 						</div>
 						
 					</div>
-					<div class="col-lg-2"></div>
-				</div>
+				<div class="col-lg-2"></div>
 			</div>
+		</div>
 
-
-			<div style="margin-bottom: 15px;">
-				<div class="row">
-					<div class="col-lg-2"></div>
+				<div style="margin-bottom: 15px;">
+			<div class="row">
+				<div class="col-lg-2"></div>
 					<div class="col-lg-3 centered">
 						<img src="assets/img/hato.jpg" width="100" height="100" alt="">
 					</div>
@@ -245,7 +339,18 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 							<div class="col-lg-10 centered">
 								<dl id="acMenu">
 									<dt><span class="glyphicon glyphicon-share-alt" style="margin-left:400px;"></span></dt>
-									<dd><textarea class="form-control" rows="" ></textarea></dd>
+									<dd><textarea class="form-control" rows="">
+										<?php 
+											$randum_id=rand(1,6);
+											$sql = sprintf('SELECT * FROM posts WHERE id='.$randum_id);
+											$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+											$table = mysqli_fetch_assoc($record);
+											$message = $table['message'];
+											echo $message;
+										?>
+										</textarea>
+										<button class="btnr" type="button"  style="float:right;">Go!</button>
+									</dd>
 								</dl>
 							</div>
 							<div class="col-lg-1 centered">
@@ -254,9 +359,9 @@ $fp = @fopen("counter1.txt", "r+") or die("Counter Error");
 						</div>
 						
 					</div>
-					<div class="col-lg-2"></div>
-				</div>
+				<div class="col-lg-2"></div>
 			</div>
+		</div>
 
 
 		</div>
